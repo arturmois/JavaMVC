bf5aa19483272e88d8b983d76b7947301938fe71
Artur Moises <arturmoiscontato@gmail.com>
2024-05-20 17:44:55 -0300

    feat(view): crate cadastro and item views

--- src/locadora/view/ViewMain.java
+++ src/locadora/view/ViewMain.java
@@ -144,10 +144,8 @@ public class ViewMain extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ViewMain().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new ViewMain().setVisible(true);
         });
     }
 
